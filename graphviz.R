978b7e16bf9248810d7ecef267fb36dc5e0e4607
jake <jlweissman@gmail.com>
2019-11-05 15:32:20 -0500

    vir hostid

--- graphviz.R
+++ graphviz.R
@@ -26,3 +26,38 @@ plot(bac2,vertex.size=0.001)
 dev.off()
 
 plot(vir2,vertex.size=0.001,vertex.label="")
+
+
+################ By Host Taxid
+
+
+library(dplyr)
+library(igraph)
+
+setwd("~/Host_Phage_Interactions/data/")
+x <- read.delim("Network_Data_v1.tsv",sep="\t",header=T,stringsAsFactors = F)
+head(x)
+
+x <- x %>% subset(select=c(virus.tax.id,host.tax.id)) %>% subset(host.tax.id!="")
+grph <- graph_from_edgelist(as.matrix(x), directed = F)
+# plot(grph,vertex.size=0.001,vertex.label="")
+
+grph2 <- delete.vertices(grph, degree(grph)==0)
+# plot(grph2,vertex.size=0.001,vertex.label="")
+V(grph2)$type <- trimws(V(grph2)$name) %in% as.character(x$virus.tax.id)
+projections <- bipartite_projection(grph2)
+
+bac <- projections[[1]]
+vir <- projections[[2]]
+bac2 <- delete.vertices(bac,degree(bac)==0)
+vir2 <- delete.vertices(vir,degree(vir)==0)
+
+setwd("~/Host_Phage_Interactions/images/")
+pdf("hosttaxid_virsharing.pdf",height=5,width=5)
+plot(bac2,vertex.size=0.001)
+dev.off()
+
+
+
+
+
