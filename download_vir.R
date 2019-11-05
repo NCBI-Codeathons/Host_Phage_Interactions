290b71284ba12e1cba82a32b3a2e9a5b0286c1a2
jake <jlweissman@gmail.com>
2019-11-05 12:30:07 -0500

    lol execute

--- download_vir.R
+++ download_vir.R
@@ -8,9 +8,11 @@ seqs <- unique(readLines("Viral_Genbank_Accesions.txt"))# %>% unique()
 # id <- entrez_search(db="nuccore",term=seqs[1])
 # summ <- entrez_summary(db="nuccore",id=id$ids)
 
-setwd("~/")
+setwd("~/vir")
 getseq <- function(seqi){
   id <- entrez_search(db="nuccore",term=seqi)
   write(entrez_fetch(db="nuccore",id=id$ids,rettype = "fasta"),
         file=paste0(seqi,".fasta"))
 }
+
+lapply(seqs,getseq)
