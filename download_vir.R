927c6bef90dba04468c530801e1cc50ae3f3265c
jake <jlweissman@gmail.com>
2019-11-05 12:25:41 -0500

    rentrez

--- download_vir.R
+++ download_vir.R
@@ -1,10 +1,12 @@
 
 
-install.packages("rentrez")
+#install.packages("rentrez")
 library(rentrez)
 
 setwd("~/Host_Phage_Interactions/data/")
-seqs <- readLines("Viral_Genbank_Accesions.txt")
+seqs <- readLines("Viral_Genbank_Accesions.txt") %>% unique()
+# id <- entrez_search(db="nuccore",term=seqs[1])
+# summ <- entrez_summary(db="nuccore",id=id$ids)
 
 setwd("~/")
 getseq <- function(seqi){
