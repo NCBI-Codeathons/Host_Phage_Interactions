ca5276c92ed4900aa7d5e8fae323c42e645206a1
jake <jlweissman@gmail.com>
2019-11-06 09:12:32 -0500

    efetch vir

---
+++ format_virsearch_query.R
@@ -0,0 +1,19 @@
+
+library(rentrez)
+
+setwd("~/Host_Phage_Interactions/data/")
+l <- readLines("Full_Viruses_Taxonomy_IDs.txt")
+l2 <- paste0("txid",l,"[Organism]") %>% unique()
+
+# write(l2[1:2000],file="Full_Viruses_Taxonomy_IDs_1.txt")
+# write(l2[2001:4000],file="Full_Viruses_Taxonomy_IDs_2.txt")
+# write(l2[4001:6000],file="Full_Viruses_Taxonomy_IDs_3.txt")
+# write(l2[6001:8000],file="Full_Viruses_Taxonomy_IDs_4.txt")
+# write(l2[8001:9998],file="Full_Viruses_Taxonomy_IDs_5.txt")
+
+efetch_cmds <- paste0('cd ~/vir; esearch -db "nucleotide" -query ',
+       l2,
+       ' |efetch -format fasta > ',
+       paste0(l,".fasta"))
+
+lapply(efetch_cmds,system)
--- graphviz.R
+++ graphviz.R
@@ -6,7 +6,7 @@ setwd("~/Host_Phage_Interactions/data/")
 x <- read.delim("Network_Data_v1.tsv",sep="\t",header=T,stringsAsFactors = F)
 head(x)
 
-x <- x %>% subset(select=c(virus.tax.id,Genus)) %>% subset(Genus!="")
+x <- x %>% subset(select=c(virus.tax.id,Genus)) %>% subset(Genus!="")%>% unique()
 grph <- graph_from_edgelist(as.matrix(x), directed = F)
 plot(grph,vertex.size=0.001,vertex.label="")
 
@@ -27,25 +27,29 @@ dev.off()
 
 plot(vir2,vertex.size=0.001,vertex.label="")
 
+grph3 <- delete.vertices(grph, degree(grph)<2)
+V(grph3)$type <- trimws(V(grph3)$name) %in% as.character(x$virus.tax.id)
+projections <- bipartite_projection(grph3)
+vir <- projections[[2]]
+plot(vir,vertex.size=1)
+
+x$Genus[x$virus.tax.id%in%trimws(V(vir)$name)]
+y <- x[x$virus.tax.id%in%trimws(V(vir)$name),]
 
 ################ By Host Taxid
 
 
-library(dplyr)
-library(igraph)
 
 setwd("~/Host_Phage_Interactions/data/")
 x <- read.delim("Network_Data_v1.tsv",sep="\t",header=T,stringsAsFactors = F)
 head(x)
 
 x <- x %>% subset(select=c(virus.tax.id,host.tax.id)) %>% subset(host.tax.id!="")
-x$virus.tax.id <- as.character(x$virus.tax.id) %>% paste0("V.",.)
-x$host.tax.id <- as.character(x$host.tax.id) %>% paste0("H.",.)
 grph <- graph_from_edgelist(as.matrix(x), directed = F)
-# plot(grph,vertex.size=0.001,vertex.label="")
+plot(grph,vertex.size=0.001,vertex.label="")
 
 grph2 <- delete.vertices(grph, degree(grph)==0)
-# plot(grph2,vertex.size=0.001,vertex.label="")
+plot(grph2,vertex.size=0.001,vertex.label="")
 V(grph2)$type <- trimws(V(grph2)$name) %in% as.character(x$virus.tax.id)
 projections <- bipartite_projection(grph2)
 
@@ -56,7 +60,7 @@ vir2 <- delete.vertices(vir,degree(vir)==0)
 
 setwd("~/Host_Phage_Interactions/images/")
 pdf("hosttaxid_virsharing.pdf",height=5,width=5)
-plot(bac2,vertex.size=0.001,vertex.label.cex=0.25)
+plot(bac2,vertex.size=0.001)
 dev.off()
 
 
